7205383fd1cb13a17e1d7319ed4ba39fe07c1f42
vitoac-22 <victoracaro13@hotmail.com>
2024-04-29 22:31:50 -0500

    Adecuaciones, en el código para que aparezca la probabilidad y esperanza matemática, luego del cuadro de valores a ingresar.

--- DISTRIBUCIONES_PROBABILIDAD/server.R
+++ DISTRIBUCIONES_PROBABILIDAD/server.R
@@ -25,7 +25,7 @@ function(input, output) {
     
     abline(v = n * p, lty = 3, col = "blue")
     
-    text(x = 0.7, y = max(f_x), labels = paste("Probabilidad:", round(prob, 4)), pos = 1)
+    # text(x = 0.7, y = max(f_x), labels = paste("Probabilidad:", round(prob, 4)), pos = 1)
   })
   
   output$plot2_binom <- renderPlot({
@@ -42,14 +42,19 @@ function(input, output) {
     
     abline(h = 1, lty = 3, col = "blue")
   })
-  output$expectation <- renderText({
-    paste("El valor de la esperanza E(X) es igual a: ", input$n_binom * input$p_binom)
+  output$expectation_prob_binom <- renderText({
+    paste("El valor de la P(X) es igual a: ", round(dbinom(input$x_binom, input$n_binom, input$p_binom),4))
   })
+  output$expectation <- renderText({
+    paste("El valor de E(X) es igual a: ", input$n_binom * input$p_binom)
+  })
+  
   
   observeEvent(input$submit_geom, {
     n <- input$n_geom
     x <- input$x_geom
-    prob <- input$prob_geom
+    prob <- as.numeric(input$prob_geom)
+    prob <- eval(parse(text = prob))
     
     # Función de densidad
     x_dgeom <- seq(1, n, by = 1)
@@ -73,8 +78,48 @@ function(input, output) {
            type = "b", col = "black")
       abline(h = 1, col = "blue", lty = 3)
     })
+    output$expectation_prob_geom <- renderText({
+      paste("El valor de la P(X) es igual a: ", round(dgeom(input$x_geom - 1, prob = input$prob_geom),4))
+    })
     output$expectation_geom <- renderText({
-      paste("El valor de la esperanza E(X) es igual a: ", 1/prob)
+      paste("El valor de E(X) es igual a: ", round(1/prob,4))
+    })
+  })
+  
+  observeEvent(input$submit_pois, {
+    n <- input$n_pois
+    val_aleatoria <- input$val_aleatoria_pois
+    lambda <- input$lambda_pois
+    
+    # Definición de intentos, el cual no se especifica porque tiene dal infinito
+    # n <- Inf
+    
+    # Gráfica de la función de densidad
+    x_dpois <- seq(0, n, by = 1)
+    y_dpois <- dpois(x_dpois, lambda = lambda)
+    
+    output$density_plot_pois <- renderPlot({
+      plot(x_dpois, y_dpois, main = "Función Densidad - Distribución Poisson",
+           xlab = 'Valores de x', ylab = 'Valores de f(x)',
+           type = "b", col = "black")
+      abline(v = lambda, col= "blue", lty = 3)
+      text(x = 0.7, y = 0.7, labels = paste("Probabilidad:", round(dpois(x=val_aleatoria, lambda = lambda), 4)), pos = 1)
+    })
+    
+    # Función de distribución acumulada
+    y_ppois <- ppois(x_dpois, lambda = lambda)
+    
+    output$distribution_plot_pois <- renderPlot({
+      plot(x_dpois, y_ppois, main = "Función Distribución Poisson",
+           xlab = 'Valores de x', ylab = 'Valores de F(x)',
+           type = "b", col = "black")
+      abline(h = 1, col = "blue", lty = 3)
+    })
+    output$expectation_prob_pois <- renderText({
+      paste("El valor de P(X) es igual a: ",round(dpois(x=input$val_aleatoria_pois ,lambda = input$lambda_pois),4))
+    })
+    output$expectation_pois <- renderText({
+      paste("El valor de E(X) es igual a: ",round(input$lambda_pois,4))
     })
   })
 }
